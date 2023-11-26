a4ec5ab7e62cb9120c51a3a36af8ddd27318c37a
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2023-11-26 22:50:13 +0500

    5 misolgacha ishlandi

---
+++ script.js
@@ -0,0 +1,57 @@
+//1-misol
+// let a = +prompt("Birinchi son");
+// let b = +prompt("ikkinchi son");
+
+// if (a > b) {
+//   console.log(a ** 2);
+// } else {
+//   console.log(b ** 2);
+// }
+
+
+//2-misol
+// let x = +prompt("Birinchi son");
+// let y = +prompt("ikkinchi son");
+// let z = +prompt("uchinchi son");
+
+// console.log(Math.max(x, y, z));
+
+
+//2-yo'li
+// let x = +prompt("Birinchi son");
+// let y = +prompt("ikkinchi son");
+// let z = +prompt("uchinchi son");
+
+// let max = x;
+// if (max < y) {
+//   max = y;
+// }
+// if (max < z) {
+//   max = z;
+// }
+// console.log(max);
+
+
+
+//3-misol
+// let a = prompt("Sonni kiriting");
+
+// if (a % 2 == 0) {
+//   console.log("Juft son");
+// } else {
+//   console.log("Toq son");
+// }
+
+// 4-misol
+// let a = prompt("Sonni kiriting");
+
+// if (a % 5 == 0) {
+//   console.log("Bu son beshga karrali");
+// } else {
+//   console.log("Bu son beshga karrali emas");
+// }
+
+// 5-misol
+let a = prompt("Sonni kiriting");
+a / 10;
+console.log(a);

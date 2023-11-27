bbbae8577432e455e2d2a90566d0ff40a9b7a1ff
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2023-11-27 17:19:09 +0500

    4-oy birinchi dars vazifalari bajarildi

--- script.js
+++ script.js
@@ -52,6 +52,58 @@
 // }
 
 // 5-misol
-let a = prompt("Sonni kiriting");
-a / 10;
-console.log(a);
+// let a = +prompt("Sonni kiriting");
+// son = Math.trunc(a / 10);
+// console.log(son);
+
+// 6-misol
+// let a = +prompt("Sonni kiriting");
+// son = a % 10;
+// console.log(son);
+
+// 7-misol
+// let a = +prompt("Sonni kiriting");
+// onlar = Math.trunc(a / 10);
+// birlar = a % 10;
+
+// if (onlar > birlar) {
+//   console.log("Rost");
+// } else {
+//   console.log("Yolg'on");
+// }
+
+// 8-misol
+// let a = +prompt("Sonni kiriting");
+// son = a % 10;
+
+// if (son % 2 == 0) {
+//   console.log("Rost");
+// } else {
+//   console.log("Yolg'on");
+// }
+
+// 9-misol
+// let a = +prompt("Sonni kiriting");
+// son = Math.trunc(a / 100);
+// console.log(son);
+
+
+
+
+// 10-misol
+// let a = +prompt("Sonni kiriting");
+// son100 = Math.trunc(a / 100);
+// son10 = Math.trunc(a / 10);
+// son1 = a % 10;
+
+// let max = son1;
+
+// if (max < son10) {
+//   max = son10;
+// }
+// if (max < son100) {
+//   max = son100;
+// }
+// console.log(max);
+
+// 10 misol chiqmadi!!!

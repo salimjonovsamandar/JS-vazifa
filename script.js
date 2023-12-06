7a7504edadaa1070ee43e520771719bec3feeb00
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2023-12-06 22:52:05 +0500

    6 dars vazifasi bajarildi

--- script.js
+++ script.js
@@ -493,3 +493,198 @@
 //   }
 // }
 // console.log(res);
+
+////////////////////////////////////////////////5 vazifa////////////////////////////////////////////////
+
+//1-misol  
+// let  n = prompt("Ixtiyoriy son kiriting")
+// sum = 0;
+// let i = 1;
+// while (i <= n) {
+//   if (i % 2 == 0) {
+//     sum += i;
+//   }
+//   i++;
+// }
+// console.log(sum);
+
+//2-misol
+// let  n = prompt("Ixtiyoriy son kiriting")
+// sum = 1;
+// let i = 1;
+// while (i <= n) {
+//   if (i % 3 == 0) {
+//     sum *= i;
+//   }
+//   i++;
+// }
+// console.log(sum);
+
+//3-misol
+// let  a = prompt("A sonni kiriting")
+// let  b = prompt("B sonni kiriting")
+// counter = 0;
+// let i = a;
+// while (i <= b) {
+//   if (i % 3 == 0 && i % 7 == 0) {
+//     counter ++
+//   }
+//   i++;
+// }
+// console.log(counter);
+
+//4-misol
+// let n = +prompt("Ixtiyoriy son kiriting");
+// let i = 1;
+// let counter = 0;
+// let res;
+// while (i <= n) {
+//   if (n % i == 0) {
+//     counter++;
+//   }
+//   i++;
+// }
+// if (counter == 2) {
+//   res = "Tub son";
+// } else {
+//   res = "Tub son emas";
+// }
+// console.log(res);
+
+//5-misol
+// let son = +prompt("Sonni kiriting");
+// let sum = 0;
+// while (son >= 1) {
+//   sum = sum + (son % 10);
+//   son = Math.trunc(son / 10);
+// }
+// console.log(sum);
+
+//6-misol
+// let n = +prompt("Ixtiyoriy 3 xonali son kiriting");
+// let b = n % 10;
+// let o = Math.trunc(n / 10) % 10;
+// let y = Math.trunc(n / 100) % 10;
+// let str = "";
+// switch (y) {
+//   case 1:
+//     str = "bir yuz ";
+//     break;
+//   case 2:
+//     str = "ikki yuz ";
+//     break;
+//   case 3:
+//     str = "uch yuz ";
+//     break;
+//   case 4:
+//     str = "to'rt yuz ";
+//     break;
+//   case 5:
+//     str = "besh yuz ";
+//     break;
+//   case 6:
+//     str = "olti yuz ";
+//     break;
+//   case 7:
+//     str = "yetti yuz ";
+//     break;
+//   case 8:
+//     str = "sakkiz yuz ";
+//     break;
+//   case 9:
+//     str = "to'qqiz yuz ";
+//     break;
+
+//   default:
+//     str = "";
+//     break;
+// }
+// switch (o) {
+//   case 1:
+//     str += "o'n ";
+//     break;
+//   case 2:
+//     str += "yigirma ";
+//     break;
+//   case 3:
+//     str += "o'ttiz ";
+//     break;
+//   case 4:
+//     str += "qirq ";
+//     break;
+//   case 5:
+//     str += "ellik ";
+//     break;
+//   case 6:
+//     str += "oltmish ";
+//     break;
+//   case 7:
+//     str += "yetmish ";
+//     break;
+//   case 8:
+//     str += "sakson ";
+//     break;
+//   case 9:
+//     str += "to'qson ";
+//     break;
+//   default:
+//     str += "";
+//     break;
+// }
+// switch (b) {
+//   case 1:
+//     srt += "bir";
+//     break;
+//   case 2:
+//     str += "ikki";
+//     break;
+//   case 3:
+//     str += "uch";
+//     break;
+//   case 4:
+//     str += "to'rt";
+//     break;
+//   case 5:
+//     str += "besh";
+//     break;
+//   case 6:
+//     str += "olti";
+//     break;
+//   case 7:
+//     str += "yetti";
+//     break;
+//   case 8:
+//     str += "sakkiz";
+//     break;
+//   case 9:
+//     str += "to'qqiz";
+//     break;
+//   default:
+//     str += "";
+//     break;
+// }
+// console.log(str);
+
+//7-misol
+// let a = +prompt("Ixtiyoriy son kiriting");
+// let b = +prompt("Ixtioriy son kiriting");
+// let z = prompt("Ixtiyoriy matematik operatorni kiriting");
+// let result;
+// switch (z) {
+//   case "+":
+//     result = a + b;
+//     break;
+//   case "-":
+//     result = a - b;
+//     break;
+//   case "*":
+//     result = a * b;
+//     break;
+//   case "/":
+//     result = a / b;
+//     break;
+//   default:
+//     result = 0;
+//     break;
+// }
+// console.log(result);

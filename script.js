451e27850e048ae8680ec24d71a34f1d3dfe39b3
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2023-12-08 21:23:31 +0500

    7 vazifa bajarildi

--- script.js
+++ script.js
@@ -494,9 +494,9 @@
 // }
 // console.log(res);
 
-////////////////////////////////////////////////5 vazifa////////////////////////////////////////////////
+////////////////////////////////////////////////6 vazifa////////////////////////////////////////////////
 
-//1-misol  
+//1-misol
 // let  n = prompt("Ixtiyoriy son kiriting")
 // sum = 0;
 // let i = 1;
@@ -688,3 +688,309 @@
 //     break;
 // }
 // console.log(result);
+
+////////////////////////////////////////////////7 vazifa////////////////////////////////////////////////
+//---------------------------------//1-misol
+// function number(a, b, c) {
+//   let max;
+//   if (a >= b) {
+//     max = a;
+//   } else {
+//     max = b;
+//   }
+//   if (c >= max) {
+//     max = c;
+//   }
+//   return max;
+// }
+// console.log(number(1, 2, 9));
+
+// const number = function (a, b, c) {
+//   let max;
+//   if (a >= b) {
+//     max = a;
+//   } else {
+//     max = b;
+//   }
+//   if (c >= max) {
+//     max = c;
+//   }
+//   return max;
+// };
+// console.log(number(1, 2, 9));
+
+// const number = (a, b, c) => {
+//   let max;
+//   if (a >= b) {
+//     max = a;
+//   } else {
+//     max = b;
+//   }
+//   if (c >= max) {
+//     max = c;
+//   }
+//   return max;
+// };
+// console.log(number(1, 2, 9));
+
+//---------------------------------//2-misol
+// function juft(a, b) {
+//   let sum = 0;
+//   for (let i = a; i <= b; i++) {
+//     if (i % 2 == 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+// console.log(juft(5, 10));
+
+// const juft = function (a, b) {
+//   let sum = 0;
+//   for (let i = a; i <= b; i++) {
+//     if (i % 2 == 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// };
+// console.log(juft(5, 10));
+
+// const juft = (a, b) => {
+//   let sum = 0;
+//   for (let i = a; i <= b; i++) {
+//     if (i % 2 == 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// };
+// console.log(juft(5, 10));
+
+//---------------------------------//3-misol
+
+// function sum(a) {
+//   if (a > 99 && a > 1000) {
+//     return "Bu uch xonali son emas";
+//   } else {
+//     let b = a % 10;
+//     let o = Math.trunc(a / 10) % 10;
+//     let y = Math.trunc(a / 100) % 10;
+//     return b + o + y;
+//   }
+// }
+// console.log(sum(155));
+
+// const sum = function (a) {
+//   if (a > 99 && a > 1000) {
+//     return "Bu uch xonali son emas";
+//   } else {
+//     let b = a % 10;
+//     let o = Math.trunc(a / 10) % 10;
+//     let y = Math.trunc(a / 100) % 10;
+//     return b + o + y;
+//   }
+// };
+// console.log(sum(155));
+
+// const sum = (a) => {
+//   if (a > 99 && a > 1000) {
+//     return "Bu uch xonali son emas";
+//   } else {
+//     let b = a % 10;
+//     let o = Math.trunc(a / 10) % 10;
+//     let y = Math.trunc(a / 100) % 10;
+//     return b + o + y;
+//   }
+// };
+// console.log(sum(155));
+
+//---------------------------------//4-misol
+// function sum(a) {
+//   if (a > 99 && a > 1000) {
+//     return "Bu uch xonali son emas";
+//   } else {
+//     let res = 0;
+//     let b = a % 10;
+//     let o = Math.trunc(a / 10) % 10;
+//     let y = Math.trunc(a / 100) % 10;
+//     if (b % 2 == 0) {
+//       res += b;
+//     }
+//     if (o % 2 == 0) {
+//       res += o;
+//     }
+//     if (y % 2 == 0) {
+//       res += y;
+//     }
+//     return res;
+//   }
+// }
+// console.log(sum(256));
+
+// const sum = function (a) {
+//   if (a > 99 && a > 1000) {
+//     return "Bu uch xonali son emas";
+//   } else {
+//     let res = 0;
+//     let b = a % 10;
+//     let o = Math.trunc(a / 10) % 10;
+//     let y = Math.trunc(a / 100) % 10;
+//     if (b % 2 == 0) {
+//       res += b;
+//     }
+//     if (o % 2 == 0) {
+//       res += o;
+//     }
+//     if (y % 2 == 0) {
+//       res += y;
+//     }
+//     return res;
+//   }
+// };
+// console.log(sum(256));
+
+// const sum = (a) => {
+//   if (a > 99 && a > 1000) {
+//     return "Bu uch xonali son emas";
+//   } else {
+//     let res = 0;
+//     let b = a % 10;
+//     let o = Math.trunc(a / 10) % 10;
+//     let y = Math.trunc(a / 100) % 10;
+//     if (b % 2 == 0) {
+//       res += b;
+//     }
+//     if (o % 2 == 0) {
+//       res += o;
+//     }
+//     if (y % 2 == 0) {
+//       res += y;
+//     }
+//     return res;
+//   }
+// };
+// console.log(sum(216));
+
+//---------------------------------//5-misol
+// function sum(a) {
+//   let res = 1;
+//   for (let i = 1; i <= a; i++) {
+//     if (a % 3 == 0 && a % 5 == 0) {
+//       continue;
+//     }
+//     res *= i;
+//   }
+//   return res;
+// }
+// console.log(sum(5));
+
+// const sum = function (a) {
+//   let res = 1;
+//   for (let i = 1; i <= a; i++) {
+//     if (a % 3 == 0 && a % 5 == 0) {
+//       continue;
+//     }
+//     res *= i;
+//   }
+//   return res;
+// };
+// console.log(sum(5));
+
+// const sum = (a) => {
+//   let res = 1;
+//   for (let i = 1; i <= a; i++) {
+//     if (a % 3 == 0 && a % 5 == 0) {
+//       continue;
+//     }
+//     res *= i;
+//   }
+//   return res;
+// };
+// console.log(sum(5));
+
+//---------------------------------//6-misol
+// function sum(a) {
+//   let res = 0;
+//   for (let i = 1; i <= a; i++) {
+//     if (i % 7 == 0 || i % 5 == 0) {
+//       res++;
+//     }
+//   }
+//   return res;
+// }
+// console.log(sum(11));
+
+// const sum = function (a) {
+//   let res = 0;
+//   for (let i = 1; i <= a; i++) {
+//     if (i % 7 == 0 || i % 5 == 0) {
+//       res++;
+//     }
+//   }
+//   return res;
+// };
+// console.log(sum(11));
+
+// const sum = (a) => {
+//   let res = 0;
+//   for (let i = 1; i <= a; i++) {
+//     if (i % 7 == 0 || i % 5 == 0) {
+//       res++;
+//     }
+//   }
+//   return res;
+// };
+// console.log(sum(11));
+
+//---------------------------------//7-misol
+// function res(n) {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     let counter = 0;
+//     for (let j = 1; j <= i; j++) {
+//       if (i % j == 0) {
+//         counter++;
+//       }
+//     }
+//     if (counter == 2) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+// console.log(res(8));
+
+// const res = function (n) {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     let counter = 0;
+//     for (let j = 1; j <= i; j++) {
+//       if (i % j == 0) {
+//         counter++;
+//       }
+//     }
+//     if (counter == 2) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// };
+// console.log(res(8));
+
+// const res = (n) => {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     let counter = 0;
+//     for (let j = 1; j <= i; j++) {
+//       if (i % j == 0) {
+//         counter++;
+//       }
+//     }
+//     if (counter == 2) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// };
+// console.log(res(8));
